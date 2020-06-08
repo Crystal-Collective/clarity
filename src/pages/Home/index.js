aa970492ffabc3e6fa31be44ed12be4392571164
peter.kuimelis <kuimeli2@illinois.edu>
2020-06-08 01:26:21 -0700

    adjust logos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,24 +4,32 @@ import { withGoogleSheets } from "react-db-google-sheets";
 import CopCardList from "../components/CopCardList";
 import MapChart from "./MapChart";
 import styled from "styled-components";
+import { ReactSVG } from "react-svg";
+import blmLogo from "../../images/blm.svg";
 import "./index.css";
 
 export const TopBar = styled.div`
   position: fixed;
   background-color: black;
   opacity: 0.9;
-  height: 75px;
+  height: 74px;
   width: 100%;
   z-index: 99;
 `;
 
 export const Logo = styled.div`
-  left: 200px;
+  left: 300px;
   top: 20px;
   position: fixed;
   color: #fce21b;
   font-size: 30px;
   font-weight: 800;
+`;
+
+export const BLM = styled.div`
+  right: 300px;
+  top: 0px;
+  position: fixed;
 `;
 
 export const Summary = styled.div`
@@ -44,6 +52,11 @@ class Home extends Component {
       <div align="center">
         <TopBar>
           <Logo>{"Crystal"}</Logo>
+          <BLM>
+            <a href="https://blacklivesmatter.com/">
+              <ReactSVG src={blmLogo} />
+            </a>
+          </BLM>
         </TopBar>
         <MapChart />
         <Summary>{cops.length + " results"}</Summary>
