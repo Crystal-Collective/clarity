3c33338ba216df22261147a46ab2b05247cafa0c
Kyle Stewart <kyle@zenput.com>
2020-06-08 20:24:31 -0700

    Add column filters

--- src/components/CopCardList.js
+++ src/components/CopCardList.js
@@ -1,13 +1,53 @@
 import React from "react";
 import styled from "styled-components";
 import CopCard from "./CopCard";
-import { useTable } from "react-table";
+import { useTable, useFilters } from "react-table";
+import CopCardFilterBar from "./CopCardFilterBar";
 
 export const Wrapper = styled.div`
   width: 1300px;
   display: flex;
   flex-wrap: wrap;
 `;
+
+// This is a custom UI for our 'between' or number range
+// filter. It uses two number boxes and filters rows to
+// ones that have values between the two
+const NumberFilter = ({ column: { filterValue = null, setFilter, id } }) => {
+  console.log("id", id);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <input
+        value={filterValue || ""}
+        type="number"
+        onChange={(e) => {
+          const val = e.target.value;
+          setFilter((old = null) => {
+            return val ? parseInt(val, 10) : null;
+          });
+        }}
+        placeholder={`Year`}
+        style={{
+          width: "70px",
+          marginRight: "0.5rem",
+        }}
+      />
+    </div>
+  );
+};
+
+const StateFilter = () => {
+  return "StateFilter";
+};
+
+const StatusFilter = () => {
+  return "StatusFilter";
+};
 
 function CopCardList(props) {
   const { cops } = props;
@@ -17,41 +57,59 @@ function CopCardList(props) {
       {
         Header: "Status",
         accessor: "status", // accessor is the "key" in the data
+        Filter: StatusFilter,
       },
       {
         Header: "State",
         accessor: "state",
+        Filter: StateFilter,
       },
       {
         Header: "Year",
         accessor: "year",
+        Filter: NumberFilter,
       },
     ],
     []
   );
 
-  console.log(cops);
-  console.log(columns);
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: () => null,
+    }),
+    []
+  );
 
-  const { rows } = useTable({ columns, data });
+  const { headerGroups, rows } = useTable(
+    { columns, data, defaultColumn },
+    useFilters
+  );
+
+  const headers = headerGroups[0].headers;
+
+  console.log(rows);
 
   return (
-    <Wrapper>
-      {rows.map((row) => {
-        const { original: cop } = row;
-
-        console.log(cop);
-        return (
-          <CopCard
-            name={cop.name}
-            location={cop.location}
-            department={cop.department}
-            incidents={cop.incidents}
-            status={cop.status}
-          />
-        );
-      })}
-    </Wrapper>
+    <>
+      <CopCardFilterBar headers={headers} />
+      <Wrapper>
+        {rows.map((row, i) => {
+          const { original: cop } = row;
+          return (
+            <CopCard
+              key={i}
+              name={cop.name}
+              location={cop.location}
+              department={cop.department}
+              incidents={cop.incidents}
+              status={cop.status}
+              year={cop.year}
+            />
+          );
+        })}
+      </Wrapper>
+    </>
   );
 }
 
