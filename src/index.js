5b29b937792a72bc9065f2412383bc399fd76069
Shundan Xiao <xiaoshundan@gmail.com>
2020-06-08 20:06:22 -0700

    Add redux and add jsconfig for absolute import (NODE_PATH has been deprecated)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import App from "./app.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
+import * as serviceWorker from './serviceWorker';
+import App from './app.js';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+const renderApp = () => ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"));
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./app.js', renderApp)
+}
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
