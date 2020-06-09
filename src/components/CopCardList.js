34978078d74d6e01fe27831fd11fb3d2e90dbe62
Kyle Stewart <kyle@zenput.com>
2020-06-08 19:18:03 -0700

    Render cards with react-table

--- src/components/CopCardList.js
+++ src/components/CopCardList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import CopCard from "./CopCard";
+import { useTable } from "react-table";
 
 export const Wrapper = styled.div`
   width: 1300px;
@@ -9,17 +10,47 @@ export const Wrapper = styled.div`
 `;
 
 function CopCardList(props) {
+  const { cops } = props;
+  const data = React.useMemo(() => cops, [cops]);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Status",
+        accessor: "status", // accessor is the "key" in the data
+      },
+      {
+        Header: "State",
+        accessor: "state",
+      },
+      {
+        Header: "Year",
+        accessor: "year",
+      },
+    ],
+    []
+  );
+
+  console.log(cops);
+  console.log(columns);
+
+  const { rows } = useTable({ columns, data });
+
   return (
     <Wrapper>
-      {props.cops.map((cop) => (
-        <CopCard
-          name={cop.name}
-          location={cop.location}
-          department={cop.department}
-          incidents={cop.incidents}
-          status={cop.status}
-        />
-      ))}
+      {rows.map((row) => {
+        const { original: cop } = row;
+
+        console.log(cop);
+        return (
+          <CopCard
+            name={cop.name}
+            location={cop.location}
+            department={cop.department}
+            incidents={cop.incidents}
+            status={cop.status}
+          />
+        );
+      })}
     </Wrapper>
   );
 }
