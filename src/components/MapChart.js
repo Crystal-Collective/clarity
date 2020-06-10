c060e2f85879a212f898270efa08b81a2f4394f0
Kyle Stewart <kgstew@gmail.com>
2020-06-10 01:03:19 -0700

    Highlight states

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -3,6 +3,18 @@ import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
+const getStyles = (stateCount, name) => {
+  return {
+    default: {
+      fill: stateCount[name] ? "#FCE21B" : "#333",
+    },
+    hover: {
+      fill: "#FCE21B",
+      outline: "none",
+    },
+  };
+};
+
 const MapChart = ({ setTooltipContent, stateCount }) => {
   return (
     <ComposableMap
@@ -14,27 +26,24 @@ const MapChart = ({ setTooltipContent, stateCount }) => {
       <Geographies geography={geoUrl}>
         {({ geographies }) => (
           <>
-            {geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                stroke="#555"
-                geography={geo}
-                fill="#333"
-                onMouseEnter={() => {
-                  const { name } = geo.properties;
-                  setTooltipContent(`${name} - ${stateCount[name]}`);
-                }}
-                onMouseLeave={() => {
-                  setTooltipContent("");
-                }}
-                style={{
-                  hover: {
-                    fill: "#FCE21B",
-                    outline: "none",
-                  },
-                }}
-              />
-            ))}
+            {geographies.map((geo) => {
+              const { name } = geo.properties;
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#555"
+                  geography={geo}
+                  fill="#333"
+                  onMouseEnter={() => {
+                    setTooltipContent(`${name} - ${stateCount[name]}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={getStyles(stateCount, name)}
+                />
+              );
+            })}
           </>
         )}
       </Geographies>
