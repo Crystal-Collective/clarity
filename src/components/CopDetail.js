4e64b1c0198132f10623be8bad85b8c5b42d29ef
Kyle Stewart <kgstew@gmail.com>
2020-06-10 20:08:28 -0700

    Add Report button on Cop Detail pane

--- src/components/CopDetail.js
+++ src/components/CopDetail.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { ReactSVG } from "react-svg";
 import { CopCard } from "components";
 import leftArrow from "../images/leftarrow.svg";
+import { COLORS } from "constants.js";
+
+const { yellow, reallyReallyDarkGrey } = COLORS;
 
 export const PanelContainer = styled.div`
   position: fixed;
@@ -13,83 +16,96 @@ export const PanelContainer = styled.div`
   transform: ${(props) => (props.show ? "translateX(0%)" : "translateX(100%)")};
   box-shadow: 0px 1.408px 21.12px rgba(52, 32, 1, 0.12);
   z-index: 99;
+  background: #fff;
 `;
 
 export const Panel = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: #fff;
-  overflow: auto;
+  padding: 64px 32px 32px;
+`;
+
+const PanelHeader = styled.div`
+  display: flex;
 `;
 
 export const Back = styled.div`
-  margin: 39px;
+  padding-top: 16px;
   color: #000;
-  display: flex;
   :hover {
     cursor: pointer;
   }
 `;
 
-export const PanelBody = styled.div`
-  text-align: left;
-  margin-top: 75px;
-`;
-
-export const Incidents = styled.table`
-  margin: 39px;
-  width: 400px;
-  font-weight: 500;
-  font-size: 14px;
-`;
-
-export const Incident = styled.tr`
-  outline: thin solid;
-`;
-
-export const IncidentFieldHeader = styled.th`
-  padding: 26px;
-  color: #9c9c9c;
-`;
-
-export const IncidentFieldData = styled.td`
-  padding: 26px;
-`;
-
 const AddReport = styled.div`
-  margin-left: 39px;
-  text-decoration: none;
+  margin-left: auto;
+  padding: 16px;
+  background-color: ${yellow};
 `;
 
 const ReportLink = styled.a`
   text-decoration: none;
+  color: ${reallyReallyDarkGrey};
+  font-weight: 700;
+`;
+
+export const PanelBody = styled.div`
+  text-align: left;
+`;
+
+const CopCardWrapper = styled.div`
+  margin: 0px 0 96px -39px;
+`;
+
+const Incidents = styled.div`
+  display: flex;
+  font-weight: 500;
+  font-size: 14px;
+  flex-direction: column;
+`;
+
+const IncidentHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  margin-bottom: 16px;
+`;
+
+const IncidentFieldHeader = styled.div`
+  flex: 1;
+  padding-left: 16px;
+  color: #9c9c9c;
+`;
+
+export const Incident = styled.div`
+  display: flex;
+  flex: 1;
+  outline: thin solid;
+`;
+
+export const IncidentFieldData = styled.div`
+  flex: 1;
+  padding: 16px;
 `;
 
 function IncidentList(props) {
   const { incidents } = props;
   return (
     <Incidents>
-      <tbody>
-        <tr>
-          <IncidentFieldHeader>{"INCIDENT"}</IncidentFieldHeader>
-          <IncidentFieldHeader>{"YEAR"}</IncidentFieldHeader>
-          <IncidentFieldHeader>{"STATUS"}</IncidentFieldHeader>
-        </tr>
-        {incidents &&
-          incidents.map((incident, i) => {
-            const { chargedOrIndicted, date, victim } = incident;
-            return (
-              <Incident key={i}>
-                <IncidentFieldData>{victim}</IncidentFieldData>
-                <IncidentFieldData>{date}</IncidentFieldData>
-                <IncidentFieldData>{chargedOrIndicted}</IncidentFieldData>
-              </Incident>
-            );
-          })}
-      </tbody>
+      <IncidentHeader>
+        <IncidentFieldHeader>{"INCIDENT"}</IncidentFieldHeader>
+        <IncidentFieldHeader>{"YEAR"}</IncidentFieldHeader>
+        <IncidentFieldHeader>{"STATUS"}</IncidentFieldHeader>
+      </IncidentHeader>
+      {incidents &&
+        incidents.map((incident, i) => {
+          const { chargedOrIndicted, date, victim } = incident;
+          return (
+            <Incident key={i}>
+              <IncidentFieldData>{victim}</IncidentFieldData>
+              <IncidentFieldData>{date}</IncidentFieldData>
+              <IncidentFieldData>{chargedOrIndicted}</IncidentFieldData>
+            </Incident>
+          );
+        })}
     </Incidents>
   );
 }
@@ -106,23 +122,29 @@ function CopDetail(props) {
   return (
     <PanelContainer show={cop}>
       <Panel>
-        <PanelBody>
+        <PanelHeader>
           <Back onClick={onClose}>
-            <ReactSVG src={leftArrow} style={{ marginRight: "8px" }} />
+            <ReactSVG
+              src={leftArrow}
+              style={{ display: "inline-block", marginRight: "8px" }}
+            />
             {"Back"}
           </Back>
-
-          <CopCard cop={cop} inline={true} />
-          <IncidentList incidents={getIndictments(cop, allCops)} />
           <AddReport>
             <ReportLink
               href="https://forms.gle/S4ohosYKn6NUQcps8"
               target="_blank"
               rel="noopener noreferrer"
             >
-              + Add a report
+              + Add Report
             </ReportLink>
           </AddReport>
+        </PanelHeader>
+        <PanelBody>
+          <CopCardWrapper>
+            <CopCard cop={cop} inline={true} />
+          </CopCardWrapper>
+          <IncidentList incidents={getIndictments(cop, allCops)} />
         </PanelBody>
       </Panel>
     </PanelContainer>
