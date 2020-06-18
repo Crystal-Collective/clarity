19e410a5c607f869e6f50955415b92a252293d43
Kyle Stewart <kyle@zenput.com>
2020-06-17 22:49:14 -0700

    Move Navigation page to material ui

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
+import clsx from "clsx";
 import styled from "styled-components";
 import Home from "./../Home";
 import About from "./../About";
 import { Route, Link } from "react-router-dom";
-import {
-  AppBar,
-  Box,
-  Link as MaterialLink,
-  Typography,
-} from "@material-ui/core";
+import { AppBar, Box, Grid, Link as MaterialLink } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
-const yellow = "#fce21b";
-const reallyReallyDarkGrey = "#333";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,44 +15,34 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#181818",
     height: "74px",
   },
-  navLink: {
-    textDecoration: "none",
-    fontWeight: 700,
-    color: theme.palette.yellow,
+  content: {
+    flexGrow: 1,
+    paddingTop: "74px",
   },
   logoLink: {
     flexGrow: 1,
-    marginTop: "15px",
     color: theme.palette.yellow,
     fontSize: "30px",
     fontWeight: 800,
     textDecoration: "none",
   },
+  logoWrapper: {
+    marginTop: "12px",
+  },
+  navLink: {
+    textDecoration: "none",
+    fontWeight: 700,
+    color: theme.palette.yellow,
+  },
+  navLinkWrapper: {
+    marginTop: "24px",
+  },
+  navLinkReport: {
+    color: theme.palette.reallyReallyDarkGrey,
+    backgroundColor: theme.palette.yellow,
+    padding: "12px 16px 12px 16px",
+  },
 }));
-
-const NavButton = styled.div`
-  margin-left: 24px;
-  margin-top: 24px;
-  color: ${yellow};
-`;
-
-const AboutNavButton = styled(NavButton)`
-  margin-left: auto;
-`;
-
-const Feedback = styled(NavButton)``;
-
-const Contribute = styled(NavButton)``;
-
-const Report = styled(NavButton)`
-  margin-top: 14px;
-  background-color: ${yellow};
-  padding: 12px 16px 14px 16px;
-`;
-
-const Content = styled.div`
-  padding-top: 74px;
-`;
 
 const Navigation = () => {
   const classes = useStyles();
@@ -67,58 +50,80 @@ const Navigation = () => {
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
-        <MaterialLink
-          underline={"none"}
-          className={classes.logoLink}
-          component={Link}
-          to="/"
-        >
-          Crystal
-        </MaterialLink>
-        <MaterialLink
-          underline={"none"}
-          className={classes.navLink}
-          component={Link}
-          to={"/about"}
-        >
-          About
-        </MaterialLink>
-        <MaterialLink
-          underline={"none"}
-          className={classes.navLink}
-          href="https://join.slack.com/t/crystalpolice/shared_invite/zt-essyn1xu-G5CAEuWgvSaZ04iIQ11FCQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Discuss on Slack
-        </MaterialLink>
-        <MaterialLink
-          underline={"none"}
-          className={classes.navLink}
-          href="https://docs.google.com/spreadsheets/d/1_8sFcTwqlBvVgd7XTekIdBGQnMdyAnB5qxuKObwdauw/edit#gid=0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Give Feedback
-        </MaterialLink>
-        <MaterialLink
-          underline={"none"}
-          className={classes.navLink}
-          href="https://forms.gle/S4ohosYKn6NUQcps8"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          + Add Report
-        </MaterialLink>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Box className={classes.logoWrapper}>
+              <MaterialLink
+                underline={"none"}
+                className={classes.logoLink}
+                component={Link}
+                to="/"
+              >
+                Crystal
+              </MaterialLink>
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Box className={classes.navLinkWrapper}>
+              <MaterialLink
+                underline={"none"}
+                className={classes.navLink}
+                component={Link}
+                to={"/about"}
+              >
+                About
+              </MaterialLink>
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Box className={classes.navLinkWrapper}>
+              <MaterialLink
+                underline={"none"}
+                className={classes.navLink}
+                href="https://join.slack.com/t/crystalpolice/shared_invite/zt-essyn1xu-G5CAEuWgvSaZ04iIQ11FCQ"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Discuss on Slack
+              </MaterialLink>
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Box className={classes.navLinkWrapper}>
+              <MaterialLink
+                underline={"none"}
+                className={classes.navLink}
+                href="https://docs.google.com/spreadsheets/d/1_8sFcTwqlBvVgd7XTekIdBGQnMdyAnB5qxuKObwdauw/edit#gid=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Give Feedback
+              </MaterialLink>
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Box className={clsx(classes.navLinkWrapper, classes.reportBox)}>
+              <MaterialLink
+                underline={"none"}
+                className={clsx(classes.navLink, classes.navLinkReport)}
+                href="https://forms.gle/S4ohosYKn6NUQcps8"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                + Add Report
+              </MaterialLink>
+            </Box>
+          </Grid>
+        </Grid>
       </AppBar>
-      <Content style={{ flexGrow: "1" }}>
+      <Box className={classes.content}>
         <Route exact={true} path="/">
           <Home />
         </Route>
         <Route exact={true} path="/about">
           <About />
         </Route>
-      </Content>
+      </Box>
     </div>
   );
 };
