d5cfd38087620bd9b30f57267d525c198c1dbae3
Kyle Stewart <kyle@zenput.com>
2020-06-17 23:16:26 -0700

    Setup hamburger menu and break points for navigation

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import clsx from "clsx";
-import styled from "styled-components";
 import Home from "./../Home";
 import About from "./../About";
 import { Route, Link } from "react-router-dom";
 import { AppBar, Box, Grid, Link as MaterialLink } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,10 +14,28 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     backgroundColor: "#181818",
     height: "74px",
+    padding: "0 16px",
   },
   content: {
     flexGrow: 1,
     paddingTop: "74px",
+  },
+  hamburgerIcon: {
+    color: theme.palette.yellow,
+  },
+  hamburgerMenu: {
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+    },
+    display: "none",
+  },
+  hamburgerMenuWrapper: {
+    marginTop: "16px",
+  },
+  inlineMenuItems: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
   },
   logoLink: {
     flexGrow: 1,
@@ -28,6 +46,7 @@ const useStyles = makeStyles((theme) => ({
   },
   logoWrapper: {
     marginTop: "12px",
+    flexGrow: 1,
   },
   navLink: {
     textDecoration: "none",
@@ -50,8 +69,8 @@ const Navigation = () => {
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
+        <Grid container spacing={2}>
+          <Grid item xs={5}>
             <Box className={classes.logoWrapper}>
               <MaterialLink
                 underline={"none"}
@@ -63,55 +82,68 @@ const Navigation = () => {
               </MaterialLink>
             </Box>
           </Grid>
-          <Grid item xs>
-            <Box className={classes.navLinkWrapper}>
-              <MaterialLink
-                underline={"none"}
-                className={classes.navLink}
-                component={Link}
-                to={"/about"}
-              >
-                About
-              </MaterialLink>
-            </Box>
+          <Grid item container xs className={classes.inlineMenuItems}>
+            <Grid item xs>
+              <Box className={classes.navLinkWrapper}>
+                <MaterialLink
+                  underline={"none"}
+                  className={classes.navLink}
+                  component={Link}
+                  to={"/about"}
+                >
+                  About
+                </MaterialLink>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box className={classes.navLinkWrapper}>
+                <MaterialLink
+                  underline={"none"}
+                  className={classes.navLink}
+                  href="https://join.slack.com/t/crystalpolice/shared_invite/zt-essyn1xu-G5CAEuWgvSaZ04iIQ11FCQ"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Discuss on Slack
+                </MaterialLink>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box className={classes.navLinkWrapper}>
+                <MaterialLink
+                  underline={"none"}
+                  className={classes.navLink}
+                  href="https://docs.google.com/spreadsheets/d/1_8sFcTwqlBvVgd7XTekIdBGQnMdyAnB5qxuKObwdauw/edit#gid=0"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Give Feedback
+                </MaterialLink>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box className={clsx(classes.navLinkWrapper, classes.reportBox)}>
+                <MaterialLink
+                  underline={"none"}
+                  className={clsx(classes.navLink, classes.navLinkReport)}
+                  href="https://forms.gle/S4ohosYKn6NUQcps8"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  + Add Report
+                </MaterialLink>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs>
-            <Box className={classes.navLinkWrapper}>
-              <MaterialLink
-                underline={"none"}
-                className={classes.navLink}
-                href="https://join.slack.com/t/crystalpolice/shared_invite/zt-essyn1xu-G5CAEuWgvSaZ04iIQ11FCQ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Discuss on Slack
-              </MaterialLink>
-            </Box>
-          </Grid>
-          <Grid item xs>
-            <Box className={classes.navLinkWrapper}>
-              <MaterialLink
-                underline={"none"}
-                className={classes.navLink}
-                href="https://docs.google.com/spreadsheets/d/1_8sFcTwqlBvVgd7XTekIdBGQnMdyAnB5qxuKObwdauw/edit#gid=0"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Give Feedback
-              </MaterialLink>
-            </Box>
-          </Grid>
-          <Grid item xs>
-            <Box className={clsx(classes.navLinkWrapper, classes.reportBox)}>
-              <MaterialLink
-                underline={"none"}
-                className={clsx(classes.navLink, classes.navLinkReport)}
-                href="https://forms.gle/S4ohosYKn6NUQcps8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                + Add Report
-              </MaterialLink>
+          <Grid
+            item
+            container
+            xs
+            className={classes.hamburgerMenu}
+            justify="flex-end"
+          >
+            <Box className={classes.hamburgerMenuWrapper}>
+              <MenuIcon fontSize={"large"} className={classes.hamburgerIcon} />
             </Box>
           </Grid>
         </Grid>
