bf63c186a1aa0129568f2efe754376d4d07cef5b
Kyle Stewart <kyle@zenput.com>
2020-06-17 23:27:25 -0700

    Split out NavigationHeader to its own component

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -1,65 +1,18 @@
 import React from "react";
-import clsx from "clsx";
 import Home from "./../Home";
 import About from "./../About";
-import { Route, Link } from "react-router-dom";
-import { AppBar, Box, Grid, Link as MaterialLink } from "@material-ui/core";
+import { Route } from "react-router-dom";
+import { Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
+import NavigationHeader from "components/NavigationHeader";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  appBar: {
-    backgroundColor: "#181818",
-    height: "74px",
-    padding: "0 16px",
-  },
   content: {
     flexGrow: 1,
     paddingTop: "74px",
-  },
-  hamburgerIcon: {
-    color: theme.palette.yellow,
-  },
-  hamburgerMenu: {
-    [theme.breakpoints.down("sm")]: {
-      display: "flex",
-    },
-    display: "none",
-  },
-  hamburgerMenuWrapper: {
-    marginTop: "16px",
-  },
-  inlineMenuItems: {
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-  logoLink: {
-    flexGrow: 1,
-    color: theme.palette.yellow,
-    fontSize: "30px",
-    fontWeight: 800,
-    textDecoration: "none",
-  },
-  logoWrapper: {
-    marginTop: "12px",
-    flexGrow: 1,
-  },
-  navLink: {
-    textDecoration: "none",
-    fontWeight: 700,
-    color: theme.palette.yellow,
-  },
-  navLinkWrapper: {
-    marginTop: "24px",
-  },
-  navLinkReport: {
-    color: theme.palette.reallyReallyDarkGrey,
-    backgroundColor: theme.palette.yellow,
-    padding: "12px 16px 12px 16px",
   },
 }));
 
@@ -68,86 +21,7 @@ const Navigation = () => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.appBar}>
-        <Grid container spacing={2}>
-          <Grid item xs={5}>
-            <Box className={classes.logoWrapper}>
-              <MaterialLink
-                underline={"none"}
-                className={classes.logoLink}
-                component={Link}
-                to="/"
-              >
-                Crystal
-              </MaterialLink>
-            </Box>
-          </Grid>
-          <Grid item container xs className={classes.inlineMenuItems}>
-            <Grid item xs>
-              <Box className={classes.navLinkWrapper}>
-                <MaterialLink
-                  underline={"none"}
-                  className={classes.navLink}
-                  component={Link}
-                  to={"/about"}
-                >
-                  About
-                </MaterialLink>
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box className={classes.navLinkWrapper}>
-                <MaterialLink
-                  underline={"none"}
-                  className={classes.navLink}
-                  href="https://join.slack.com/t/crystalpolice/shared_invite/zt-essyn1xu-G5CAEuWgvSaZ04iIQ11FCQ"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Discuss on Slack
-                </MaterialLink>
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box className={classes.navLinkWrapper}>
-                <MaterialLink
-                  underline={"none"}
-                  className={classes.navLink}
-                  href="https://docs.google.com/spreadsheets/d/1_8sFcTwqlBvVgd7XTekIdBGQnMdyAnB5qxuKObwdauw/edit#gid=0"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Give Feedback
-                </MaterialLink>
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box className={clsx(classes.navLinkWrapper, classes.reportBox)}>
-                <MaterialLink
-                  underline={"none"}
-                  className={clsx(classes.navLink, classes.navLinkReport)}
-                  href="https://forms.gle/S4ohosYKn6NUQcps8"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  + Add Report
-                </MaterialLink>
-              </Box>
-            </Grid>
-          </Grid>
-          <Grid
-            item
-            container
-            xs
-            className={classes.hamburgerMenu}
-            justify="flex-end"
-          >
-            <Box className={classes.hamburgerMenuWrapper}>
-              <MenuIcon fontSize={"large"} className={classes.hamburgerIcon} />
-            </Box>
-          </Grid>
-        </Grid>
-      </AppBar>
+      <NavigationHeader />
       <Box className={classes.content}>
         <Route exact={true} path="/">
           <Home />
