6395d8e1d4062bc4770187b661707dcdde918c04
Kyle Stewart <kyle@zenput.com>
2020-06-17 22:19:31 -0700

    Add material-ui theme provider and theme

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import React from "react";
 import GoogleSheetsProvider from "react-db-google-sheets";
 import { Navigation } from "pages";
 import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import theme from "./theme";
 
 const App = () => (
   <GoogleSheetsProvider>
     <Router style={{ height: "100%" }}>
-      <Navigation style={{ height: "100%" }} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navigation style={{ height: "100%" }} />
+      </ThemeProvider>
     </Router>
   </GoogleSheetsProvider>
 );
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+const theme = createMuiTheme({
+  palette: {
+    yellow: "#fce21b",
+    reallyReallyDarkGrey: "#333",
+  },
+});
+
+export default theme;
