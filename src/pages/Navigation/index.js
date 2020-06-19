a6dd97f246d90eacd8b20034654a23e6383ed6c8
Kyle Stewart <kyle@zenput.com>
2020-06-18 19:43:02 -0700

    Remove root styles to allow About and Home to be styled separately

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -2,35 +2,19 @@ import React from "react";
 import Home from "./../Home";
 import About from "./../About";
 import { Route } from "react-router-dom";
-import { Box } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import NavigationHeader from "components/NavigationHeader";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  content: {
-    flexGrow: 1,
-    paddingTop: "74px",
-  },
-}));
-
 const Navigation = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <>
       <NavigationHeader />
-      <Box className={classes.content}>
-        <Route exact={true} path="/">
-          <Home />
-        </Route>
-        <Route exact={true} path="/about">
-          <About />
-        </Route>
-      </Box>
-    </div>
+      <Route exact={true} path="/">
+        <Home />
+      </Route>
+      <Route exact={true} path="/about">
+        <About />
+      </Route>
+    </>
   );
 };
 
